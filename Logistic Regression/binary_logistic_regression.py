ef3112787a6aa60931ea1027873e006c3225d61d
sayemraza <46274376+sayemraza@users.noreply.github.com>
2019-12-26 11:29:34 +0530

    Update binary_logistic_regression.py

--- Logistic Regression/binary_logistic_regression.py
+++ Logistic Regression/binary_logistic_regression.py
@@ -1,33 +1,50 @@
-from math import exp
-def sigmoid(x):
-    return 1 / (1 + exp(-x))
+import numpy as np
 
-from math import log
-def CrossEntropy(yHat, y):
-    if y == 1:
-        return -log(yHat)
-    else:
-        return -log(1 - yHat)
+class binary_logistic_regression:
 
-def get_weights(W,X,yHat,y,learning_rate):
-    gradient = np.dot(X.T,(yHat - y))
-    avg_gradient = np.array(gradient)/X.shape[0]
-    avg_gradient = avg_gradient * learning_rate
-    W = W-avg_gradient
-    return W
+    def __init__(self,X_train,y_train):
+        self.X_train = X_train
+        self.y_train = y_train.reshape(y_train.shape[0],1)
+        self.n_features = self.X_train.shape[1]
+    
+    def next_batch(self,X, y, batchSize):
+        for i in np.arange(0, X.shape[0], batchSize):
+            yield (X[i:i + batchSize], y[i:i + batchSize])
 
-def get_loss(W,X,y):
-    z = np.dot(X, W)
-    yHat = np.array([sigmoid(each) for each in z], dtype = np.float64)
-    ce = []
-    for i in range(len(yHat)):
-        ce.append(CrossEntropy(yHat[i],y[i]))
-    loss = sum(ce)/yHat.shape[0]
-    return loss, yHat
 
-def train(epochs,n_features,X_train,y_train,learning_rate=0.1):
-    W = np.random.normal(size=n_features)
-    for epoch in range(epochs):
-        loss,yHat = get_loss(W,X_train,y_train)
-        W = get_weights(W,X_train,yHat,y_train,learning_rate = learning_rate)
-    return W,loss
+    def sigmoid(self,z):
+        return 1 / (1 + np.exp(-z))
+
+    def CrossEntropy(self,yHat, y):
+        if y == 1:
+            return -np.log(yHat)
+        else:
+            return -np.log(1 - yHat)
+
+    def update_weights(self,yHat):
+        gradient = np.dot(self.X_train.T,(yHat - self.y_train))
+        avg_gradient = np.array(gradient)/self.X_train.shape[0]
+        avg_gradient = avg_gradient * self.learning_rate
+        self.W = self.W-avg_gradient
+
+
+    def fit(self,learning_rate=0.01, epochs = 10000):
+        self.learning_rate = learning_rate
+        self.W = np.random.normal(size=(self.n_features,1))
+        for epoch in range(epochs):
+            z = np.dot(self.X_train, self.W)
+            yHat = np.array([self.sigmoid(each) for each in z], dtype = np.float64)
+            ce = []
+            for i in range(len(yHat)):
+                ce.append(self.CrossEntropy(yHat[i],self.y_train[i]))
+            self.loss = sum(ce)/yHat.shape[0][0]
+            self.update_weights(yHat)
+            if epoch%100 == 0:
+                print('Epoch: ', epoch, 'Loss: ', self.loss)
+    
+    def predict(self,X_test):
+        z = np.dot(self.X_train, self.W)
+        yHat = np.array([self.sigmoid(each) for each in z], dtype = np.float64)
+        yHat[yHat>0.5]=1
+        yHat[yHat<0.5]=0
+        return yHat
